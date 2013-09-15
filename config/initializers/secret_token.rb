b88c2a5443d420b85f8fe30d1e9286a39f7ea643
Michael Morrissey <mikehmorrissey@gmail.com>
2013-09-15 12:36:11 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IolabsTasks2::Application.config.secret_key_base = '71a788b4565cfd0e5a1bf94ff71e7c8e88c5b7b55503e798ea5271b2aadc1c0b3726abb6ecdd17d462a80d985cdf91369c1c2447fa98a21758fedbba17848613'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IolabsTasks2::Application.config.session_store :cookie_store, key: '_iolabs_tasks_2_session'
