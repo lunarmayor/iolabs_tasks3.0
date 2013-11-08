160cd13b46064e3c6b0951e7dc269d63e46cbedd
Michael Morrissey <mikehmorrissey@gmail.com>
2013-11-07 20:31:10 -0600

    fix error

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,4 @@
 class Client < ActiveRecord::Base
-  has_attached_file :logo, :default_url => :default_url => ActionController::Base.helpers.asset_path("assets/missing.png")
+  has_attached_file :logo, :default_url => ActionController::Base.helpers.asset_path("assets/missing.png")
   has_many :jobs
 end
