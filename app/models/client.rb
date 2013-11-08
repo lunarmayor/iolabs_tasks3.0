8c0b48da6cf70d5ae880981f813e4d1621ce334d
Michael Morrissey <mikehmorrissey@gmail.com>
2013-11-07 22:12:47 -0600

    hey

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,4 @@
 class Client < ActiveRecord::Base
-  has_attached_file :logo, :default_url => "assets/missing-5af4bdd80ad9ae0116dee1291f7ad902.png"
+  has_attached_file :logo, :default_url => "/missing-5af4bdd80ad9ae0116dee1291f7ad902.png"
   has_many :jobs
 end
